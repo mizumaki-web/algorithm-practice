24394ca7da1f0edfca55b445df7771fc55edb7c3
mizumaki-web <yousei_ft@yahoo.co.jp>
2025-04-07 18:02:54 +0900

    Update B.cpp

--- ABC400/B.cpp
+++ ABC400/B.cpp
@@ -22,11 +22,15 @@ int main(){
 }
 
 /*
-問題：https://atcoder.jp/contests/abc400/tasks/abc400_b
-提出；https://atcoder.jp/contests/abc400/submissions/64523965
-入力N・Mから、Nの0乗からM乗までの和を求める問題。
+問題リンク：https://atcoder.jp/contests/abc400/tasks/abc400_b
+提出リンク；https://atcoder.jp/contests/abc400/submissions/64523965
+
+問題概要：入力N・Mから、Nの0乗からM乗までの和を求める問題。
 出力は10^9以下ならば和を、それより大きかったらinfを出力する。
 
+実行例：入力 7 3　出力 400 (1+7+49+343=400)
+
+アルゴリズム・考え方：
 10^9と大きな数が与えられるので、int型ではなくlong long型を用いる。
 
 累乗はpow関数、もしくは
@@ -37,5 +41,5 @@ for(int i = 0; i <= M; i++){
   }
 このようにすればpow関数を使わずに書くことができる(参考；https://atcoder.jp/contests/abc400/submissions/64567417)。
 
-出力をそのままするとlong long型でもオーバーフローしてしまう恐れがあるので、負の値が出力されてもinfと出力するようにした。
+long long 型でもオーバーフローが発生する場合があるため、演算結果が10^9を超える、または符号が反転した場合は "inf" を出力するようにした。
 */
